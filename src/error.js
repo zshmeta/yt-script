ab4b18c401dbf6fbc799047a5d219d99155d9296
zshmeta <zshmeta@duck.com>
2024-05-20 15:51:43 +0200

    yt-scripts

--- src/error.js
+++ src/error.js
@@ -11,7 +11,7 @@ function couldNotRetrieveTranscript(videoId) {
 }
 
 function buildErrorMessage(videoId, cause) {
-    let errorMessage = `\nCould not retrieve a transcript for the video ${settings.watchURL.replace('{video_id}', videoId)}!`;
+    let errorMessage = `\nCould not retrieve a transcript for the video ${watchURL.replace('{video_id}', videoId)}!`;
     if (cause) {
         errorMessage += ` This is most likely caused by:\n\n${cause}`;
         errorMessage += `\n\nIf you are sure that the described cause is not responsible for this error and that a transcript should be retrievable, please create an issue at https://github.com/jdepoix/youtube-transcript-api/issues. Please add which version of youtube-transcript-api you are using and provide the information needed to replicate the error. Also make sure that there are no open issues which already describe your problem!`;
