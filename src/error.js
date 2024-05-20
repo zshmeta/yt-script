5b4ab0e3f640309868b8b94286ba719bca60e247
zshmeta <zshmeta@duck.com>
2024-05-20 15:42:55 +0200

    yt-scripts

--- src/error.js
+++ src/error.js
@@ -20,7 +20,7 @@ function buildErrorMessage(videoId, cause) {
 }
 
 function youTubeRequestFailed(videoId, httpError) {
-    const error = createCouldNotRetrieveTranscript(videoId);
+    const error = couldNotRetrieveTranscript(videoId);
     error.reason = httpError.message;
     error.cause = `Request to YouTube failed: ${error.reason}`;
     error.name = 'YouTubeRequestFailed';
@@ -28,77 +28,77 @@ function youTubeRequestFailed(videoId, httpError) {
 }
 
 function videoUnavailable(videoId) {
-    const error = createCouldNotRetrieveTranscript(videoId);
+    const error = couldNotRetrieveTranscript(videoId);
     error.cause = 'The video is no longer available';
     error.name = 'VideoUnavailable';
     return error;
 }
 
 function invalidVideoId(videoId) {
-    const error = createCouldNotRetrieveTranscript(videoId);
+    const error = couldNotRetrieveTranscript(videoId);
     error.cause = 'You provided an invalid video id. Make sure you are using the video id and NOT the url!\n\nDo NOT run: `YouTubeTranscriptApi.getTranscript("https://www.youtube.com/watch?v=1234")`\nInstead run: `YouTubeTranscriptApi.getTranscript("1234")`';
     error.name = 'InvalidVideoId';
     return error;
 }
 
 function tooManyRequests(videoId) {
-    const error = createCouldNotRetrieveTranscript(videoId);
+    const error = couldNotRetrieveTranscript(videoId);
     error.cause = 'YouTube is receiving too many requests from this IP and now requires solving a captcha to continue. One of the following things can be done to work around this:\n- Manually solve the captcha in a browser and export the cookie. Read here how to use that cookie with youtube-transcript-api: https://github.com/jdepoix/youtube-transcript-api#cookies\n- Use a different IP address\n- Wait until the ban on your IP has been lifted';
     error.name = 'TooManyRequests';
     return error;
 }
 
 function transcriptsDisabled(videoId) {
-    const error = createCouldNotRetrieveTranscript(videoId);
+    const error = couldNotRetrieveTranscript(videoId);
     error.cause = 'Subtitles are disabled for this video';
     error.name = 'TranscriptsDisabled';
     return error;
 }
 
 function noTranscriptAvailable(videoId) {
-    const error = createCouldNotRetrieveTranscript(videoId);
+    const error = couldNotRetrieveTranscript(videoId);
     error.cause = 'No transcripts are available for this video';
     error.name = 'NoTranscriptAvailable';
     return error;
 }
 
 function notTranslatable(videoId) {
-    const error = createCouldNotRetrieveTranscript(videoId);
+    const error = couldNotRetrieveTranscript(videoId);
     error.cause = 'The requested language is not translatable';
     error.name = 'NotTranslatable';
     return error;
 }
 
 function translationLanguageNotAvailable(videoId) {
-    const error = createCouldNotRetrieveTranscript(videoId);
+    const error = couldNotRetrieveTranscript(videoId);
     error.cause = 'The requested translation language is not available';
     error.name = 'TranslationLanguageNotAvailable';
     return error;
 }
 
 function cookiePathInvalid(videoId) {
-    const error = createCouldNotRetrieveTranscript(videoId);
+    const error = couldNotRetrieveTranscript(videoId);
     error.cause = 'The provided cookie file was unable to be loaded';
     error.name = 'CookiePathInvalid';
     return error;
 }
 
 function cookiesInvalid(videoId) {
-    const error = createCouldNotRetrieveTranscript(videoId);
+    const error = couldNotRetrieveTranscript(videoId);
     error.cause = 'The cookies provided are not valid (may have expired)';
     error.name = 'CookiesInvalid';
     return error;
 }
 
 function failedToCreateConsentCookie(videoId) {
-    const error = createCouldNotRetrieveTranscript(videoId);
+    const error = couldNotRetrieveTranscript(videoId);
     error.cause = 'Failed to automatically give consent to saving cookies';
     error.name = 'FailedToCreateConsentCookie';
     return error;
 }
 
 function noTranscriptFound(videoId, requestedLanguageCodes, transcriptData) {
-    const error = createCouldNotRetrieveTranscript(videoId);
+    const error = couldNotRetrieveTranscript(videoId);
     error._requestedLanguageCodes = requestedLanguageCodes;
     error._transcriptData = transcriptData;
     error.cause = `No transcripts were found for any of the requested language codes: ${requestedLanguageCodes.join(', ')}\n\n${transcriptData}`;
